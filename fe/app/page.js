8049cd1c3e128baedfe9b4da46d2ab4ca6f01b28
james ballanger <jamesballanger@trentu.ca>
2024-02-19 18:14:04 -0500

    api requests implemented

--- fe/app/page.js
+++ fe/app/page.js
@@ -1,14 +1,27 @@
+import { useEffect, useState } from "react"
+
 const { BASE_API_URL } = process.env
 
 export default async function Home() {
-  const getStudents = await fetch(BASE_API_URL + "/students")
-  // const getCourses = await fetch(BASE_API_URL + "/courses")
-  // const getResults = await fetch(BASE_API_URL + "/results")
-  // const postStudents = await fetch(method="POST", BASE_API_URL + "/students")
-  // const postCourses = await fetch(method="POST", BASE_API_URL + "/courses")
-  // const postResults = await fetch(method="POST", BASE_API_URL + "/results")
+  const [data, setData] = useState([])
+  const [url, setUrl] = useState([])
+  const [postUrl, setPostUrl] = useState([])
 
+  const get = useEffect(async (url) => {
+    const response = await fetch(BASE_API_URL + url)
+    data = await response.json()
 
+    setData(data)
+  }, [url])
+
+  const post = useEffect(async (postUrl) => {
+    const response = await fetch(BASE_API_URL + postUrl, method="POST")
+    data = await response.json()
+
+    setData(data)
+  }, [postUrl])
+
+  console.log("RESULT", students)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
