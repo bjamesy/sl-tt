35806ed0f65b2ed2e7a3113afe73899065d16319
james ballanger <jamesballanger@trentu.ca>
2024-02-19 20:07:53 -0500

    cors fix in fetch header

--- fe/app/page.js
+++ fe/app/page.js
@@ -1,32 +1,30 @@
+"use client"
 import { useEffect, useState } from "react"
 
 const { BASE_API_URL } = process.env
 
-export default async function Home() {
+export default function Home() {
   const [data, setData] = useState([])
   const [url, setUrl] = useState([])
-  const [postUrl, setPostUrl] = useState([])
 
-  const get = useEffect(async (url) => {
-    const response = await fetch(BASE_API_URL + url)
-    data = await response.json()
+  useEffect(() => {
+    const fetchData = async () => {
+        const response = await fetch(BASE_API_URL + "/students", {
+            mode: 'no-cors',
+        })
 
-    setData(data)
-  }, [url])
+        setData(response)
+    }
 
-  const post = useEffect(async (postUrl) => {
-    const response = await fetch(BASE_API_URL + postUrl, method="POST")
-    data = await response.json()
+    fetchData()
+  }, [])
 
-    setData(data)
-  }, [postUrl])
-
-  console.log("RESULT", students)
+  console.log("RESULT", data)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         HOME PAGE
       </div>
     </main>
-  );
+  )
 }
