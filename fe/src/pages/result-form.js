63bcb91488600b46393c51c828aa4f5807b2fd83
James Ballanger <jamesballanger@jamess-air.home>
2024-02-24 15:46:56 -0500

    courses and student calls to populate dropdowns for result form page

--- fe/src/pages/result-form.js
+++ fe/src/pages/result-form.js
@@ -1,8 +1,32 @@
 "use client"
 import { useForm } from 'react-hook-form'
+import { useState, useEffect } from 'react'
 
 const ResultForm = () => {
   const { register, handleSubmit, formState: { errors } } = useForm()
+  const [courses, setCourses] = useState([])
+  const [students, setStudents] = useState([])
+
+  useEffect(() => {
+    (async () => {
+        const response = await fetch('http://127.0.0.1:5000/students')
+        if (!response.ok) throw new Error('Failed to fetch student data')
+
+        const data = await response.json()
+        setStudents(data)
+    })()
+  }, [])
+
+  useEffect(() => {
+    (async () => {
+        const response = await fetch('http://127.0.0.1:5000/courses')
+        if (!response.ok) {
+          throw new Error('Failed to fetch course data')
+        }
+        const data = await response.json()
+        setCourses(data)
+    })()
+  }, [])
 
   const postResult = async(data) => {
     const response  = await fetch(`http://127.0.0.1:5000/results`,{
@@ -16,31 +40,39 @@ const ResultForm = () => {
     <form onSubmit={handleSubmit(postResult)}>
       <h3>Add Result</h3>
       <div>
-        <label htmlFor="course">Course name:</label>
-        <input
-          type="text"
+        <label htmlFor="course">Course</label>
+        <select
           id="course"
           {...register('course', { required: true })}
-        />
-        {errors.familyName && <span>This field is required</span>}
+        >
+        { courses.map(course => <option key={course[1]} value={course[1]}>{course[0]}</option> )}
+        </select>
+        {errors.course && <span>This field is required</span>}
       </div>
       <div>
-        <label htmlFor="student">Student name:</label>
-        <input
-          type="text"
+        <label htmlFor="student">Student</label>
+        <select
           id="student"
           {...register('student', { required: true })}
-        />
-        {errors.dob && <span>This field is required</span>}
+        >
+        { students.map(student => <option key={student[4]} value={student[4]}>{student[0]}</option> )}
+        </select>
+        {errors.student && <span>This field is required</span>}
       </div>
       <div>
         <label htmlFor="score">Score:</label>
-        <input
-          type="text"
+        <select
           id="score"
           {...register('score', { required: true })}
-        />
-        {errors.email && <span>This field is required</span>}
+        >
+          <option key="A" value="A">A</option>
+          <option key="B" value="B">B</option>
+          <option key="C" value="C">C</option>
+          <option key="D" value="D">D</option>
+          <option key="E" value="E">E</option>
+          <option key="F" value="F">F</option>
+        </select>
+        {errors.score && <span>This field is required</span>}
       </div>
       <button type="submit">Submit</button>
     </form>
